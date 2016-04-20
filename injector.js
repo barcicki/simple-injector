929557269c3dd7f21b8acf42bf31df4658eb55ae
Artur Barcicki <barcicki@gmail.com>
2016-04-21 00:02:07 +0200

    css injection
    added basic css injection
     extracted injection methods for scripts and styles
     decided to use indexOf instead of regexp

--- injector.js
+++ injector.js
@@ -1,9 +1,69 @@
-const escapeStringRegexp = require('escape-string-regexp');
-
 const JS_FILE = /\.js$/;
-const JS_REPLACEMENT_START = '<!-- injector:js -->';
+const CSS_FILE = /\.css$/;
+const REPLACEMENT_START_JS = '<!-- injector:js -->';
+const REPLACEMENT_START_CSS = '<!-- injector:css -->';
 const REPLACEMENT_END = '<!-- endinjector -->';
 const JOIN_DELIMITER = '';
+
+function replaceStyles(html, files) {
+    const startPos = html.indexOf(REPLACEMENT_START_CSS);
+    const endPos = html.indexOf(REPLACEMENT_END, startPos);
+
+    if (startPos === -1) {
+        return html;
+    }
+
+    const cssFiles = files.filter(fileName => CSS_FILE.test(fileName));
+
+    if (!cssFiles.length) {
+        return html;
+    }
+
+    const injectedTags = cssFiles
+        .map(fileName => `<link rel="stylesheet" href="${fileName}" type="text/css">`)
+        .join(JOIN_DELIMITER);
+
+    const precedingHtml = html.slice(0, startPos + REPLACEMENT_START_CSS.length);
+    let succeedingHtml;
+
+    if (endPos > startPos) {
+        succeedingHtml = html.slice(endPos);
+    } else {
+        succeedingHtml = REPLACEMENT_END + html.slice(startPos + REPLACEMENT_START_CSS.length);
+    }
+
+    return precedingHtml + injectedTags + succeedingHtml;
+}
+
+function replaceScripts(html, files) {
+    const startPos = html.indexOf(REPLACEMENT_START_JS);
+    const endPos = html.indexOf(REPLACEMENT_END, startPos);
+
+    if (startPos === -1) {
+        return html;
+    }
+
+    const jsFiles = files.filter(fileName => JS_FILE.test(fileName));
+
+    if (!jsFiles.length) {
+        return html;
+    }
+
+    const injectedTags = jsFiles
+        .map(fileName => `<script src="${fileName}"></script>`)
+        .join(JOIN_DELIMITER);
+
+    const precedingHtml = html.slice(0, startPos + REPLACEMENT_START_JS.length);
+    let succeedingHtml;
+
+    if (endPos > startPos) {
+        succeedingHtml = html.slice(endPos);
+    } else {
+        succeedingHtml = REPLACEMENT_END + html.slice(startPos + REPLACEMENT_START_JS.length);
+    }
+
+    return precedingHtml + injectedTags + succeedingHtml;
+}
 
 function baseInjector(html, scriptsAndStyles) {
     if (!html) {
@@ -14,17 +74,7 @@ function baseInjector(html, scriptsAndStyles) {
         return html;
     }
 
-    const scriptStart = escapeStringRegexp(JS_REPLACEMENT_START);
-    const replacementEnd = escapeStringRegexp(REPLACEMENT_END);
-    const scriptReplacement = new RegExp(`${scriptStart}([\\s\\S]*)${replacementEnd}`, 'g');
-
-    const scriptsTags = scriptsAndStyles
-        .filter(fileName => JS_FILE.test(fileName))
-        .map(fileName => `<script src="${fileName}"></script>`);
-
-    const scripts = `${JS_REPLACEMENT_START}${scriptsTags.join(JOIN_DELIMITER)}${REPLACEMENT_END}`;
-
-    return html.replace(scriptReplacement, scripts);
+    return replaceStyles(replaceScripts(html, scriptsAndStyles), scriptsAndStyles);
 }
 
 module.exports = {
--- spec/base.injector.spec.js
+++ spec/base.injector.spec.js
@@ -1,7 +1,7 @@
 /* eslint max-len: ["off"] */
 const { baseInjector } = require('../injector');
 
-const SAMPLE_HTML_PAGE = '<html><head><title>Test</title><!--injector:css --><!-- endinjector --></head><body><!-- injector:js --><!-- endinjector --></body></html>';
+const SAMPLE_HTML_PAGE = '<html><head><title>Test</title><!-- injector:css --><!-- endinjector --></head><body><!-- injector:js --><!-- endinjector --></body></html>';
 
 describe('Base injector', () => {
     it('should throw when no html content are provided', () => {
@@ -16,8 +16,8 @@ describe('Base injector', () => {
 
     it('should inject js files', () => {
         const initialHtml = SAMPLE_HTML_PAGE;
-        const scripts = ['file.js'];
-        const targetHtml = '<html><head><title>Test</title><!--injector:css --><!-- endinjector --></head><body><!-- injector:js --><script src="file.js"></script><!-- endinjector --></body></html>';
+        const scripts = ['file1.js', 'file2.js'];
+        const targetHtml = '<html><head><title>Test</title><!-- injector:css --><!-- endinjector --></head><body><!-- injector:js --><script src="file1.js"></script><script src="file2.js"></script><!-- endinjector --></body></html>';
 
         const finalHtml = baseInjector(initialHtml, scripts);
 
@@ -28,10 +28,32 @@ describe('Base injector', () => {
         const initialHtml = SAMPLE_HTML_PAGE;
         const firstScriptSet = ['file1.js'];
         const secondScriptSet = ['file2.js'];
-        const targetHtml = '<html><head><title>Test</title><!--injector:css --><!-- endinjector --></head><body><!-- injector:js --><script src="file2.js"></script><!-- endinjector --></body></html>';
+        const targetHtml = '<html><head><title>Test</title><!-- injector:css --><!-- endinjector --></head><body><!-- injector:js --><script src="file2.js"></script><!-- endinjector --></body></html>';
 
         const middleHtml = baseInjector(initialHtml, firstScriptSet);
         const finalHtml = baseInjector(middleHtml, secondScriptSet);
+
+        expect(finalHtml).toBe(targetHtml);
+    });
+
+    it('should inject css files', () => {
+        const initialHtml = SAMPLE_HTML_PAGE;
+        const styles = ['file1.css', 'file2.css'];
+        const targetHtml = '<html><head><title>Test</title><!-- injector:css --><link rel="stylesheet" href="file1.css" type="text/css"><link rel="stylesheet" href="file2.css" type="text/css"><!-- endinjector --></head><body><!-- injector:js --><!-- endinjector --></body></html>';
+
+        const finalHtml = baseInjector(initialHtml, styles);
+
+        expect(finalHtml).toBe(targetHtml);
+    });
+
+    it('should inject css in place of previously inject css files', () => {
+        const initialHtml = SAMPLE_HTML_PAGE;
+        const firstStylesSet = ['file1.css'];
+        const secondStylesSet = ['file2.css'];
+        const targetHtml = '<html><head><title>Test</title><!-- injector:css --><link rel="stylesheet" href="file2.css" type="text/css"><!-- endinjector --></head><body><!-- injector:js --><!-- endinjector --></body></html>';
+
+        const middleHtml = baseInjector(initialHtml, firstStylesSet);
+        const finalHtml = baseInjector(middleHtml, secondStylesSet);
 
         expect(finalHtml).toBe(targetHtml);
     });
