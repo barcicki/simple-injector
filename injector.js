5938ec89471d6f0def781a21a392863e4ec0e176
Artur Barcicki <barcicki@gmail.com>
2016-04-20 22:58:38 +0200

    js injection
    added simple js injection functionality

--- injector.js
+++ injector.js
@@ -1,13 +1,30 @@
+const escapeStringRegexp = require('escape-string-regexp');
+
+const JS_FILE = /\.js$/;
+const JS_REPLACEMENT_START = '<!-- injector:js -->';
+const REPLACEMENT_END = '<!-- endinjector -->';
+const JOIN_DELIMITER = '';
+
 function baseInjector(html, scriptsAndStyles) {
     if (!html) {
         throw new Error('No HTML content provided');
     }
 
-    if (!scriptsAndStyles || scriptsAndStyles.length) {
+    if (!scriptsAndStyles || !scriptsAndStyles.length) {
         return html;
     }
 
-    return html;
+    const scriptStart = escapeStringRegexp(JS_REPLACEMENT_START);
+    const replacementEnd = escapeStringRegexp(REPLACEMENT_END);
+    const scriptReplacement = new RegExp(`${scriptStart}([\\s\\S]*)${replacementEnd}`, 'g');
+
+    const scriptsTags = scriptsAndStyles
+        .filter(fileName => JS_FILE.test(fileName))
+        .map(fileName => `<script src="${fileName}"></script>`);
+
+    const scripts = `${JS_REPLACEMENT_START}${scriptsTags.join(JOIN_DELIMITER)}${REPLACEMENT_END}`;
+
+    return html.replace(scriptReplacement, scripts);
 }
 
 module.exports = {
--- spec/base.injector.spec.js
+++ spec/base.injector.spec.js
@@ -1,4 +1,7 @@
+/* eslint max-len: ["off"] */
 const { baseInjector } = require('../injector');
+
+const SAMPLE_HTML_PAGE = '<html><head><title>Test</title><!--injector:css --><!-- endinjector --></head><body><!-- injector:js --><!-- endinjector --></body></html>';
 
 describe('Base injector', () => {
     it('should throw when no html content are provided', () => {
@@ -9,5 +12,27 @@ describe('Base injector', () => {
     it('should return unmodified html if list of files is empty', () => {
         expect(baseInjector('test')).toBe('test');
         expect(baseInjector('test2', [])).toBe('test2');
+    });
+
+    it('should inject js files', () => {
+        const initialHtml = SAMPLE_HTML_PAGE;
+        const scripts = ['file.js'];
+        const targetHtml = '<html><head><title>Test</title><!--injector:css --><!-- endinjector --></head><body><!-- injector:js --><script src="file.js"></script><!-- endinjector --></body></html>';
+
+        const finalHtml = baseInjector(initialHtml, scripts);
+
+        expect(finalHtml).toBe(targetHtml);
+    });
+
+    it('should inject js in place of previously inject js files', () => {
+        const initialHtml = SAMPLE_HTML_PAGE;
+        const firstScriptSet = ['file1.js'];
+        const secondScriptSet = ['file2.js'];
+        const targetHtml = '<html><head><title>Test</title><!--injector:css --><!-- endinjector --></head><body><!-- injector:js --><script src="file2.js"></script><!-- endinjector --></body></html>';
+
+        const middleHtml = baseInjector(initialHtml, firstScriptSet);
+        const finalHtml = baseInjector(middleHtml, secondScriptSet);
+
+        expect(finalHtml).toBe(targetHtml);
     });
 });
